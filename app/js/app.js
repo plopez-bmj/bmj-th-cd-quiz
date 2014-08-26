c944f2ac17873e498c0a8bd420f420d33f7c9320
plopez-bmj <plopez@bmj.com>
2014-08-26 14:50:49 +0100

    Add questions and tests

--- app/js/app.js
+++ app/js/app.js
@@ -10,6 +10,7 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/answers', {templateUrl: 'partials/answers.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/questions', {templateUrl: 'partials/questions.html', controller: 'MyCtrl1'});
+  $routeProvider.otherwise({redirectTo: '/questions'});
 }]);
--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -6,21 +6,54 @@ describe('my app', function() {
 
   browser.get('index.html');
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+  it('should automatically redirect to /questions when location hash/fragment is empty', function() {
+    expect(browser.getLocationAbsUrl()).toMatch("/questions");
   });
 
 
-  describe('view1', function() {
+  describe('answers', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/view1');
+      browser.get('index.html#/answers');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] ol li')).first().getText()).
-      toMatch(/Question 1/);
+    it('should have selected d for question 1', function() {
+      expect(element.all(by.id('a1')).first().getText()).
+      toContain('-d. independent');
+    });
+
+    it('should have selected d for question 2', function() {
+      expect(element.all(by.id('a2')).first().getText()).
+      toContain('-b. False');
+    });
+
+    it('should have selected d for question 3', function() {
+      expect(element.all(by.id('a3')).first().getText()).
+      toContain('-c. Being transparent and open');
+    });
+
+    it('should have selected d for question 4', function() {
+      expect(element.all(by.id('a4')).first().getText()).
+      toContain('-a. We take pride in our people');
+    });
+
+    it('should have selected d for question 5', function() {
+      expect(element.all(by.id('a5')).first().getText()).
+      toContain('-b. Do, well, not, all');
+    });
+
+    it('should have selected d for question 6', function() {
+      expect(element.all(by.id('a6')).first().getText()).
+      toContain('-a. Users');
+    });
+    it('should have selected d for question 7', function() {
+      expect(element.all(by.id('a7')).first().getText()).
+      toContain('-e. healthcare');
+    });
+    it('should have selected d for question 8', function() {
+      expect(element.all(by.id('a8')).first().getText()).
+      toContain('-d. courage');
     });
 
   });
