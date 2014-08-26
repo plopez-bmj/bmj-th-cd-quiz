b69cd4a6585b0c32c73f951133ea8bc6fadeff5d
BMJLNg <lng@bmj.com>
2014-08-27 00:21:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,35 +5,5 @@ http.createServer(function(request, response) {
   response.write("Hello World");
   response.end();
 }).listen(process.env.PORT || 8888);
+console.log("process.env.PORT:" + process.env.PORT);
 
-/// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-}); 
-
-
-module.exports = app;
