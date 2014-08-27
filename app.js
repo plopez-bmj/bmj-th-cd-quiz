83ec4f9ab2d5f81dc83d13397bf6d5065d9fd00f
plopez-bmj <plopez@bmj.com>
2014-08-27 16:13:37 +0100

    Not applicable

--- app.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("Hello World");
-  response.end();
-}).listen(process.env.PORT || 8888);
-console.log("process.env.PORT:" + process.env.PORT);
-
